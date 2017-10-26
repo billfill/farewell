9ecbb251cbfbe4ef0ed43a7ac4dffb90086b6111
Billy.Liao <billfulo@gmail.com>
2017-10-26 17:49:05 +0800

    test

--- src/js-dev/original/index.js
+++ src/js-dev/original/index.js
@@ -35,9 +35,14 @@ $(document).ready(function () {
     var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
     function isFacebookApp() {
-        var ua = navigator.userAgent || navigator.vendor || window.opera;
-        return ua.indexOf("FBAN") > -1 || ua.indexOf("FBAV") > -1;
+        if(iOS == true){
+            var ua = navigator.userAgent || navigator.vendor || window.opera;
+            return ua.indexOf("FBAN") > -1 || ua.indexOf("FBAV") > -1;
+        } else {
+            return false;
+        }
     }
+
     function detectmob() {
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
             return true;
